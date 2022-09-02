11a7bf1a9c0569d8930a9af73c61888a80c07178
SuperGandhi <patrickborjabueno@hotmail.com>
2022-09-02 17:49:33 -0500

    Fix bug rating

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const starsEl = document.querySelectorAll(".fa-star");
-console.log(starsEl);
+const emojisEl = document.querySelector(".fa-solid");
+const colors = ["red", "orange", "lightblue", "lightgreen", "green"]
+
+updateRating(0);
+
+starsEl.forEach((starEl, index) => {
+    starEl.addEventListener("click", () =>{
+        updateRating(index);
+    })
+});
+
+function updateRating(index){
+    starsEl.forEach((starEl, idx) => {
+        if(idx < index + 1){
+            starEl.classList.add("active");
+        }else{
+            starEl.classList.remove("active");
+        }
+    });
+
+    emojisEl.forEach((emojiEl) =>{
+        emojiEl.style.transform = `translateX(-${index * 50}px)`;
+    })
+}
